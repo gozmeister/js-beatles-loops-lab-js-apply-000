e477e75213bc420d56fe435f8effc1ad9039baee
Sam Gorick <samgorick.sg@gmail.com>
2020-02-07 09:44:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // add solution here
+function theBeatlesPlay(musician, instrument) {
+  var result = []
+  var i;
+  var str;
+  for (i=0; i < musician.length; i++) {
+    str = `${musician[i]} plays ${instrument[i]}`
+    result.push(str)
+  }
+  return result
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] += "!!!"
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number) {
+  var love = []
+  var i = 0;
+  do {
+    love.push("I love the Beatles!")
+  }
+  while (i < (15 - number)) {
+    love.push("I love the Beatles!")
+    i++
+  }
+  return love
+}
+
+var musicians = [
+  "John Lennon",
+  "Ringo Starr"]
+  
+  var instruments = [
+    "guitar",
+    "drums"]
+    
+  var jLFacts = 
+  ["He is dead",
+  "He was in the Beatles"]
+    
+
+console.log(iLoveTheBeatles(15))
